c48eda454817f78960364e3cc7658cedc0798ac6
jean <jeang51@nycstudents.net>
2025-02-11 09:20:51 -0500

    notes 2

--- notes.py
+++ notes.py
@@ -6,5 +6,22 @@ def login(password)
         print("incorrect password") 
 
 def temp(x)
-    if x >=88
-    
+    if x >=88 
+
+
+
+    #loop from 2 to Y for i in range(2,15)
+    #if factor == true add to list
+    #loop from 2 to Y for isfactor
+
+    isRich = True
+    is21 = True
+
+    def canGamble(isRich, is21):
+        if isRich == True and is21 == True:
+            print("let it ride")
+        elif isRich == False and is21 ==True:
+            print("you are too poor get out")
+        elif isrich == False or is21 == False:
+            print("you cannot play")
+

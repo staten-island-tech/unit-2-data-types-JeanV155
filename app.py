05100eab66f5257fb14f62b09190ca67743e1175
jean <jeang51@nycstudents.net>
2025-02-12 09:18:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-# Step 1: Create variables for the bill, tip, and total
-bill = input("Enter the bill amount: ")  # Bill amount will be input as a string
-tip = input("Enter the tip amount: ")    # Tip amount will be input as a string
+# Step 1: Ask the user to input a sentence
+verb1 = input("Please enter a verb: ")
+verb2 = input("please enter a verb: ")
+noun = input("please enter a noun: ")
+number = input("please enter a number: ")
+celebrity = input("please enter a celebrity")
 
-# Step 2: Convert the data types
-bill = float(bill)  # Change the bill to a float
-tip = int(tip)      # Change the tip to an integer
+# Create a sentence using f-string
+sentence = f"Today, I will {verb1} and {verb2} with {number} {noun}s  and tell {celebrity} all about it!" 
 
-# Step 3: Calculate the total
-total = bill + tip  # Total amount is the sum of bill and tip
-
-# Step 4: Print the formatted string
-print(f"The total bill is ${bill:.2f}, the tip is ${tip}, and the total amount paid is ${total:.2f}")
+# print the formatted sentence 
+print(sentence)

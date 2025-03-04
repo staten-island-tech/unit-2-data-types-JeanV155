6510d96398232fe6a6b8a678d2c4f69e276d0070
jean <jeang51@nycstudents.net>
2025-03-04 08:46:18 -0500

    test.py

---
+++ undifend.py
@@ -0,0 +1,22 @@
+price = int(input("Enter price of item one by one: "))
+finished = input("IF done, enter Done: ")
+def cost (price):
+    bill.append(price)
+    for i in bill:
+        price + i
+
+bill = []
+while finished != "Done": 
+      print(finished)
+      print(cost(price))
+
+        
+    
+
+print("final cost is: {bill}")
+ 
+for (i) in bill:
+    i + bill
+ 
+if bill > 100:
+    bill - 0.1 
